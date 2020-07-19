36beab8fdd8d32793a7d3b6e6613c104b4772450
MartinErceg <martin_7245@hotmail.co.nz>
2020-07-19 12:22:28 +1200

    MenuUI + Martins Code

---
+++ Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/CanvasChanger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasChanger : MonoBehaviour
+{
+    [SerializeField] private GameObject gameCanvas;
+    [SerializeField] private GameObject pauseCanvas;
+    [SerializeField] private GameObject endCanvas;
+    [SerializeField] private TerrainManager tm;
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        gameCanvas.SetActive(false);
+        pauseCanvas.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        gameCanvas.SetActive(true);
+        pauseCanvas.SetActive(false);
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        gameCanvas.SetActive(false);
+        endCanvas.SetActive(true);
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1;
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+        gameCanvas.SetActive(true);
+        pauseCanvas.SetActive(false);
+        endCanvas.SetActive(false);
+    }
+}
